98656364ca9ed32c7f569d4acce326baa6327bd2
Josh Carlson <magneticstain@gmail.com>
2014-05-09 19:43:46 -0400

    Added library templates

---
+++ lib/pastebien.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  Josh Carlson
+ *  Created: 5/9/14 7:39 PM
+ *  Email: jcarlson@carlso.net
+ */
+
+/*
+ *  lib/Pastebien.php - a library containing the class Pastebin() - an all-encompassing object with site-wide functions
+ * 						that other classes inherit from.
+ */
+
+class Pastebin
+{
+
+}
---
+++ lib/pastrie.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  Josh Carlson
+ *  Created: 5/9/14 7:39 PM
+ *  Email: jcarlson@carlso.net
+ */
+
+/*
+ *  lib/Pastrie.php - a library containing the class Paste() - an object representing a paste and all associated
+ * 						characteristics.
+ */
+
+class Paste
+{
+
+}
