d56d6e818576447549be35b47bbfdbd0724f574a
Josh Carlson <magneticstain@gmail.com>
2015-05-30 14:19:59 -0400

    re-adding db config file

---
+++ PrivyPaste/conf/db.php
@@ -0,0 +1,18 @@
+<?php
+
+    /**
+     *  PrivyPaste
+     *  Author: Josh Carlson
+     *  Email: jcarlson(at)carlso(dot)net
+     */
+
+    /*
+     *  conf/db.php - sets variables used for database-related functionality
+     */
+
+	// DB SETTINGS
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'privypaste');
+	define('DB_USER', 'privypaste');
+	define('DB_PASS', 'xb4To14XHK2IhY2j');
+?>
