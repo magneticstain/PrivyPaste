48f0fd363a2d928681c907006d80512878160e6e
Josh Carlson <magneticstain@gmail.com>
2015-08-15 10:24:06 -0400

    patched install script so that web files actually get copied

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ function installApplicationFiles
     # move to application directory
     cp -r ./* /opt/privypaste/src/ > /dev/null 2>&1
     cp -r *.pem /opt/privypaste/certs/ > /dev/null 2>&1
-    rsync -av PrivyPaste-master/PrivyPaste/ /opt/privypaste/web/PrivyPaste/ --exclude 'src' --exclude 'tests' > /dev/null 2>&1
+    rsync -av ./PrivyPaste/ /opt/privypaste/web/PrivyPaste/ --exclude 'src' --exclude 'tests' > /dev/null 2>&1
 
     # set perms
     # check which distro we're using
