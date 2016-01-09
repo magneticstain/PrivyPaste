2a823b604d94cca3ece87261d7b52e41b7c10737
Josh Carlson <magneticstain@gmail.com>
2016-01-09 13:55:34 -0500

    add User() class for foundation of user management

---
+++ PrivyPaste/lib/user.php
@@ -0,0 +1,18 @@
+<?php
+	namespace privypaste;
+
+	/**
+	 *  PrivyPaste
+	 *  Author: Josh Carlson
+	 *  Email: jcarlson(at)carlso(dot)net
+	 */
+
+	/*
+	 *  user.php - a class containing logic related to user management
+	 */
+
+	class User
+	{
+
+	}
+?>
